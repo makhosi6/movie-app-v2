3b3f511f6e10268810a3efaa4c1aedc3ade710d4
makhosi6 <makhosindondo@outlook.com>
2021-05-15 20:55:27 +0200

    Updates

---
+++ precache-manifest.4eedb12013170714618318b24b2cf308.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626b217ce4ed765630f71236c7b0d70b",
+    "url": "/movie-app-v2/index.html"
+  },
+  {
+    "revision": "0f5c8bdccf57247ece88",
+    "url": "/movie-app-v2/static/css/main.4a669f0e.chunk.css"
+  },
+  {
+    "revision": "228212af46073e0cddb6",
+    "url": "/movie-app-v2/static/js/2.22f7ee33.chunk.js"
+  },
+  {
+    "revision": "a13e872f1be45dec9aa498959bbec3a8",
+    "url": "/movie-app-v2/static/js/2.22f7ee33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f5c8bdccf57247ece88",
+    "url": "/movie-app-v2/static/js/main.4a7deba6.chunk.js"
+  },
+  {
+    "revision": "fe77896ec8ab87e4a546",
+    "url": "/movie-app-v2/static/js/runtime-main.cd95e4b2.js"
+  },
+  {
+    "revision": "d543319d12d2882b05da071a01a61cee",
+    "url": "/movie-app-v2/static/media/2lBOQK06tltt8SQaswgb8d657Mv.d543319d.jpg"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/movie-app-v2/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "2fe2efe63441d830b1acd106c1fe8734",
+    "url": "/movie-app-v2/static/media/Simple-Line-Icons.2fe2efe6.svg"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/movie-app-v2/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/movie-app-v2/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "f33df365d6d0255b586f2920355e94d7",
+    "url": "/movie-app-v2/static/media/Simple-Line-Icons.f33df365.eot"
+  },
+  {
+    "revision": "6d0ff45adf5666464493fc26234cc2cb",
+    "url": "/movie-app-v2/static/media/horizontal-tablet.6d0ff45a.svg"
+  },
+  {
+    "revision": "d127938bc27a9645fb5b5b830bf631aa",
+    "url": "/movie-app-v2/static/media/primary-white-d70eebe18a5eb5b166d5c1ef0796715b8d1a2cbc698f96d311d62f894ae87085.d127938b.svg"
+  }
+]);
